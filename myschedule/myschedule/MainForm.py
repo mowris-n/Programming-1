0e89e392a5626be97590c8a1182b9131425d8ae4
mowris-n <mowris.n@stu.janesville.k12.wi.us>
2024-09-24 15:16:22 -0500

    deleting unnessecary files

---
+++ myschedule/myschedule/MainForm.py
@@ -0,0 +1,72 @@
+﻿import System.Drawing
+import System.Windows.Forms
+
+from System.Drawing import *
+from System.Windows.Forms import *
+
+class MainForm(Form):
+    def __init__(self):
+        self.InitializeComponent()
+    
+    def InitializeComponent(self):
+        self._label1 = System.Windows.Forms.Label()
+        self._button1 = System.Windows.Forms.Button()
+        self._button2 = System.Windows.Forms.Button()
+        self._button3 = System.Windows.Forms.Button()
+        self.SuspendLayout()
+        # 
+        # label1
+        # 
+        self._label1.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        self._label1.Font = System.Drawing.Font("Myanmar Text", 15.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._label1.Location = System.Drawing.Point(205, 27)
+        self._label1.Name = "label1"
+        self._label1.Size = System.Drawing.Size(508, 235)
+        self._label1.TabIndex = 0
+        # 
+        # button1
+        # 
+        self._button1.Font = System.Drawing.Font("Microsoft Sans Serif", 12, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._button1.Location = System.Drawing.Point(71, 310)
+        self._button1.Name = "button1"
+        self._button1.Size = System.Drawing.Size(191, 67)
+        self._button1.TabIndex = 1
+        self._button1.Text = "Show"
+        self._button1.UseVisualStyleBackColor = True
+        self._button1.Click += self.Button1Click
+        # 
+        # button2
+        # 
+        self._button2.Font = System.Drawing.Font("Microsoft Sans Serif", 12, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._button2.Location = System.Drawing.Point(367, 310)
+        self._button2.Name = "button2"
+        self._button2.Size = System.Drawing.Size(191, 67)
+        self._button2.TabIndex = 2
+        self._button2.Text = "Clear"
+        self._button2.UseVisualStyleBackColor = True
+        # 
+        # button3
+        # 
+        self._button3.Font = System.Drawing.Font("Microsoft Sans Serif", 12, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._button3.Location = System.Drawing.Point(656, 310)
+        self._button3.Name = "button3"
+        self._button3.Size = System.Drawing.Size(191, 67)
+        self._button3.TabIndex = 3
+        self._button3.Text = "Exit"
+        self._button3.UseVisualStyleBackColor = True
+        # 
+        # MainForm
+        # 
+        self.BackColor = System.Drawing.Color.FromArgb(192, 0, 0)
+        self.ClientSize = System.Drawing.Size(971, 409)
+        self.Controls.Add(self._button3)
+        self.Controls.Add(self._button2)
+        self.Controls.Add(self._button1)
+        self.Controls.Add(self._label1)
+        self.Name = "MainForm"
+        self.Text = "myschedule"
+        self.ResumeLayout(False)
+
+
+    def Button1Click(self, sender, e):
+        show._label.Text = "1. study hall 2.Computer Applications 3."
