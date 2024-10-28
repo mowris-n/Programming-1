83fdf506a75a6f5d4df24559aad92ae4f6b6abff
mowris-n <mowris.n@stu.janesville.k12.wi.us>
2024-10-28 15:27:40 -0500

    s

---
+++ LP-2/LP-2/Program.py
@@ -0,0 +1,15 @@
+﻿weight = int(input("Enter weight in kilograms: "))
+length = int(input("Enter length in centimeters: "))
+width = int(input("Enter width in centimeters: "))
+height = int(input("Enter height in centimeters: "))
+dimension = length * width * height
+
+
+if dimension > 100000 and weight > 27:
+    print("too large and too heavy")
+elif weight > 27:
+    print("too heavy")
+elif dimension > 100000:
+    print("too large")
+
+input()
---
+++ LP4-3/LP4-3/Program.py
@@ -0,0 +1,20 @@
+﻿eggs = int(input("Enter # of eggs: "))
+price = 0.0
+cost = 0.0
+
+if eggs < 4:
+    price = 0.50
+elif eggs < 6:
+    price = 0.45
+elif eggs < 11:
+    price = 0.40
+elif eggs >= 11:
+    price = 0.35
+else:
+    print("invalid # of eggs")
+
+
+cost = eggs * price
+print("Cost is $" + str(round(cost,2)))
+
+input()
