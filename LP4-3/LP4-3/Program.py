7d0f15e879cbeee47669637dbbc8ac54ce6293d0
mowris-n <mowris.n@stu.janesville.k12.wi.us>
2024-10-29 14:56:21 -0500

    classwork

--- LP4-3/LP4-3/Program.py
+++ LP4-3/LP4-3/Program.py
@@ -1,20 +1,20 @@
-﻿eggs = int(input("Enter # of eggs: "))
+﻿eggs = int(input("Enter amount of eggs: "))
 price = 0.0
 cost = 0.0
 
-if eggs < 4:
+if eggs < 48:
     price = 0.50
-elif eggs < 6:
+elif eggs < 72:
     price = 0.45
-elif eggs < 11:
+elif eggs < 132:
     price = 0.40
-elif eggs >= 11:
+elif eggs >= 132:
     price = 0.35
 else:
-    print("invalid # of eggs")
+    print("invalid amount of eggs")
 
 
-cost = eggs * price
+cost = (eggs * price) / 12
 print("Cost is $" + str(round(cost,2)))
 
 input()
