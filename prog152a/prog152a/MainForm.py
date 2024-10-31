e59f20ab822976be529c96914851c9aa0ae8aa0c
mowris-n <mowris.n@stu.janesville.k12.wi.us>
2024-10-31 15:27:55 -0500

    cs

--- prog152a/prog152a/MainForm.py
+++ prog152a/prog152a/MainForm.py
@@ -9,99 +9,59 @@ class MainForm(Form):
         self.InitializeComponent()
     
     def InitializeComponent(self):
-        self._label1 = System.Windows.Forms.Label()
-        self._label2 = System.Windows.Forms.Label()
-        self._label3 = System.Windows.Forms.Label()
-        self._label4 = System.Windows.Forms.Label()
         self._button1 = System.Windows.Forms.Button()
         self._button2 = System.Windows.Forms.Button()
         self._button3 = System.Windows.Forms.Button()
+        self._listBox1 = System.Windows.Forms.ListBox()
         self.SuspendLayout()
-        # 
-        # label1
-        # 
-        self._label1.BackColor = System.Drawing.Color.Plum
-        self._label1.Font = System.Drawing.Font("Microsoft Sans Serif", 15.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
-        self._label1.Location = System.Drawing.Point(12, 20)
-        self._label1.Name = "label1"
-        self._label1.Size = System.Drawing.Size(154, 66)
-        self._label1.TabIndex = 0
-        self._label1.Text = "Multiple:"
-        self._label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
-        self._label1.Click += self.Label1Click
-        # 
-        # label2
-        # 
-        self._label2.BackColor = System.Drawing.Color.Violet
-        self._label2.Font = System.Drawing.Font("Microsoft Sans Serif", 15.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
-        self._label2.Location = System.Drawing.Point(202, 20)
-        self._label2.Name = "label2"
-        self._label2.Size = System.Drawing.Size(154, 66)
-        self._label2.TabIndex = 1
-        self._label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
-        # 
-        # label3
-        # 
-        self._label3.BackColor = System.Drawing.Color.Plum
-        self._label3.Font = System.Drawing.Font("Microsoft Sans Serif", 15.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
-        self._label3.Location = System.Drawing.Point(12, 110)
-        self._label3.Name = "label3"
-        self._label3.Size = System.Drawing.Size(154, 66)
-        self._label3.TabIndex = 2
-        self._label3.Text = "Sum:"
-        self._label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
-        # 
-        # label4
-        # 
-        self._label4.BackColor = System.Drawing.Color.Violet
-        self._label4.Font = System.Drawing.Font("Microsoft Sans Serif", 15.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
-        self._label4.Location = System.Drawing.Point(202, 110)
-        self._label4.Name = "label4"
-        self._label4.Size = System.Drawing.Size(154, 66)
-        self._label4.TabIndex = 3
-        self._label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         # 
         # button1
         # 
         self._button1.Font = System.Drawing.Font("Microsoft Sans Serif", 15.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
-        self._button1.Location = System.Drawing.Point(12, 318)
+        self._button1.Location = System.Drawing.Point(12, 333)
         self._button1.Name = "button1"
         self._button1.Size = System.Drawing.Size(229, 64)
         self._button1.TabIndex = 4
-        self._button1.Text = "calculate"
+        self._button1.Text = "Calculate"
         self._button1.UseVisualStyleBackColor = True
+        self._button1.Click += self.Button1Click
         # 
         # button2
         # 
         self._button2.Font = System.Drawing.Font("Microsoft Sans Serif", 15.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
-        self._button2.Location = System.Drawing.Point(292, 318)
+        self._button2.Location = System.Drawing.Point(301, 333)
         self._button2.Name = "button2"
         self._button2.Size = System.Drawing.Size(229, 64)
         self._button2.TabIndex = 5
-        self._button2.Text = "calculate"
+        self._button2.Text = "Clear"
         self._button2.UseVisualStyleBackColor = True
         # 
         # button3
         # 
         self._button3.Font = System.Drawing.Font("Microsoft Sans Serif", 15.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
-        self._button3.Location = System.Drawing.Point(574, 318)
+        self._button3.Location = System.Drawing.Point(594, 333)
         self._button3.Name = "button3"
         self._button3.Size = System.Drawing.Size(229, 64)
         self._button3.TabIndex = 6
-        self._button3.Text = "calculate"
+        self._button3.Text = "Exit"
         self._button3.UseVisualStyleBackColor = True
+        # 
+        # listBox1
+        # 
+        self._listBox1.FormattingEnabled = True
+        self._listBox1.Location = System.Drawing.Point(12, 12)
+        self._listBox1.Name = "listBox1"
+        self._listBox1.Size = System.Drawing.Size(811, 303)
+        self._listBox1.TabIndex = 7
         # 
         # MainForm
         # 
         self.BackColor = System.Drawing.Color.Thistle
         self.ClientSize = System.Drawing.Size(863, 419)
+        self.Controls.Add(self._listBox1)
         self.Controls.Add(self._button3)
         self.Controls.Add(self._button2)
         self.Controls.Add(self._button1)
-        self.Controls.Add(self._label4)
-        self.Controls.Add(self._label3)
-        self.Controls.Add(self._label2)
-        self.Controls.Add(self._label1)
         self.Name = "MainForm"
         self.Text = "prog152a"
         self.ResumeLayout(False)
@@ -112,3 +72,14 @@ class MainForm(Form):
 
     def TextBox1TextChanged(self, sender, e):
         pass
+
+    def Button1Click(self, sender, e): 
+        s = 0
+        for v in range(0, 9669,3):
+            s += v
+            self._listBox1.Items.Add(v)
+            self._listBox1.Items.Add(s)
+            
+        
+            
+            
---
+++ slices/slices/Program.py
@@ -0,0 +1,16 @@
+﻿list1 = [1, 2, 3, 4, 5]
+front = list1[0:3]
+back = list1 [3:len(list1)]
+print (front)
+print (back)
+print (front + back)
+
+hello = "Hello, world!"
+substr = hello[:5] # [0:5]
+print(substr)
+print(hello[-1])
+print(hello[-6:-1]) #[start:stop:step]
+print(hello[::-1]) # [0:len(hello):-1]
+print(hello == hello[::-1])
+
+input()
